fa59f9d52ba0fa72e4d4f349c56814fe5b74b559
Shivangi1223 <singhshivangi21805@gmail.com>
2025-06-24 00:41:59 +0530

    updated server.js

--- server/server.js
+++ server/server.js
@@ -4,8 +4,12 @@ import cors from "cors";
 import connectDB from "./configs/db.js";
 import { clerkMiddleware } from '@clerk/express'
 import clerkWebhooks from "./controllers/clerkWebhooks.js";
+import userRouter from "./routes/userRoutes.js";
+import hotelRouter from "./routes/hotelRoutes.js";
+import connectCloudinary from "./configs/cloudinary.js";
 
 connectDB() 
+connectCloudinary();
 
 const app = express()
 app.use(cors()) // Enable Cross-Origin Resource Sharing -->>
@@ -20,6 +24,8 @@ app.use(clerkMiddleware())
 //API to listen to clerk Webhooks -->>
 app.use("/api/clerk", clerkWebhooks);
 app.get('/', (req, res)=> res.send("API is working"))
+app.use('/api/user', userRouter)
+app.use('/api/hotels', hotelRouter)
 
 const PORT = process.env.PORT || 3000;
 
