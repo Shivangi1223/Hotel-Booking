4d9e3cf613a6377530868eeab24eee1eaf13de95
Shivangi1223 <singhshivangi21805@gmail.com>
2025-06-29 14:11:48 +0530

    Add in server

--- server/models/User.js
+++ server/models/User.js
@@ -17,3 +17,16 @@ const User = mongoose.model("User", userSchema)
 
 
 export default User;
+
+// import mongoose from "mongoose";
+
+// const userSchema = mongoose.Schema({
+//   username: { type: String, required: true },
+//   email: { type: String, required: true },
+//   image: { type: String, required: true },
+//   role: { type: String, enum: ["user", "hotelOwner"], default: "user" },
+//   recentSearchedCities: [{ type: String, default: [] }],
+// }, { timestamps: true });
+
+// const User = mongoose.model("User", userSchema);
+// export default User;
